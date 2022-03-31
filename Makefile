4f5479731eacd079b7ab7a60f14dba6f7a71ad14
Rolf Verberg <rv43@cornell.edu>
2022-03-31 16:40:48 -0400

    fix: bug fixes while testing and updating unit tests

--- Makefile
+++ Makefile
@@ -5,26 +5,26 @@ conda_user = chess_msnc
 
 test:
 	# Create fresh test environment
-	ksu $(conda_user) -e /usr/bin/cp environment.yml $(conda_base)/tomo_test.yml
+	ksu $(conda_user) -e /usr/bin/cp environment.yaml $(conda_base)/tomo_test.yaml
 	ksu $(conda_user) -e $(conda_base)/bin/conda env remove -n tomo_test
-	ksu $(conda_user) -e $(conda_base)/bin/conda env create -n tomo_test -f $(conda_base)/tomo_test.yml
-	# Run test
-	/usr/bin/cp tests/config.txt config.txt
-	/usr/bin/rm -rf tests/output/actual
-	/usr/bin/mkdir tests/output/actual
+	ksu $(conda_user) -e $(conda_base)/bin/conda env create -n tomo_test -f $(conda_base)/tomo_test.yaml
+	# Run tests
+	/usr/bin/rm -rf tests/test1/output/actual
+	/usr/bin/mkdir tests/test1/output/actual
+	/usr/bin/rm -rf tests/test2/output/actual
+	/usr/bin/mkdir tests/test2/output/actual
 	$(conda_base)/envs/tomo_test/bin/python -m unittest tests/test_tomo.py
 	# Remove the test environment
 	ksu $(conda_user) -e $(conda_base)/bin/conda env remove -n tomo_test
-	ksu $(conda_user) -e /usr/bin/rm $(conda_base)/tomo_test.yml
-	/usr/bin/rm config.txt
+	ksu $(conda_user) -e /usr/bin/rm $(conda_base)/tomo_test.yaml
 
 env:
-	ksu $(conda_user) -e /usr/bin/cp environment.yml $(conda_base)/tomopy.yml
+	ksu $(conda_user) -e /usr/bin/cp environment.yaml $(conda_base)/tomopy.yaml
 	ksu $(conda_user) -e $(conda_base)/bin/conda env remove -n tomopy
-	ksu $(conda_user) -e $(conda_base)/bin/conda env create -f $(conda_base)/tomopy.yml
-	ksu $(conda_user) -e /usr/bin/cp environment-spyder.yml $(conda_base)/tomopy-spyder.yml
-	ksu $(conda_user) -e $(conda_base)/bin/conda env remove -n tomopy-spyder
-	ksu $(conda_user) -e $(conda_base)/bin/conda env create -f $(conda_base)/tomopy-spyder.yml
+	ksu $(conda_user) -e $(conda_base)/bin/conda env create -f $(conda_base)/tomopy.yaml
+	ksu $(conda_user) -e /usr/bin/cp environment-galaxy.yaml $(conda_base)/tomopy-galaxy.yaml
+	ksu $(conda_user) -e $(conda_base)/bin/conda env remove -n tomopy-galaxy
+	ksu $(conda_user) -e $(conda_base)/bin/conda env create -f $(conda_base)/tomopy-galaxy.yaml
 
 clean:
 	find . -name "*~" -delete
--- tests/test_tomo.py
+++ tests/test_tomo.py
@@ -15,49 +15,53 @@ class TomoTestCase(unittest.TestCase):
 
     def test_tomo(self):
         '''
-        Run tomo on the config.txt file for testing.
-        Compare results to the saved expected output.
+        Test tomo on the config.yaml's in each of the test directories.
+        Compare results to the saved expected outputs.
         '''
 
-        # Run the tomo test
-        print(f'Generating test data for comparison...')
-        runTomo(config_file='config.txt', output_folder='tests/output/actual/', test_mode=True)
+        for i in [1, 2]:
+            with self.subTest(i=i):
+                name = f'test{str(i)}'
+                # Run test
+                print(f'Generating {name} data for comparison...')
+                runTomo(config_file=f'tests/{name}/config.yaml',
+                        output_folder=f'tests/{name}/output/actual/', test_mode=True)
 
-        # Compare each file of the test output with the expected output.
-        out_dir = os.path.join(os.path.dirname(work_dir), 'tests/output/')
-        exp_dir = os.path.join(out_dir, 'expected/')
-        act_dir = os.path.join(out_dir, 'actual/')
-        files = [f for f in os.listdir(exp_dir) if os.path.isfile(os.path.join(exp_dir, f)) and
-                f.endswith('.txt')]
-        for f in files:
-            exp_fpath = os.path.join(exp_dir, f)
-            act_fpath = os.path.join(act_dir, f)
-            self.assertTrue(os.path.isfile(os.path.join(act_dir, f)))
-            with open(exp_fpath) as f:
-                exp_items = re.findall(r'\S+', f.read())
-            with open(act_fpath) as f:
-                act_items = re.findall(r'\S+', f.read())
-            self.assertEqual(len(act_items), len(exp_items))
-            err = []
-            for (exp_item, act_item) in zip(exp_items, act_items):
-                try:
-                    e = literal_eval(exp_item)
-                    a = literal_eval(act_item)
-                except (ValueError, TypeError, SyntaxError, MemoryError):
-                    continue
-                if e and a:
-                    err.append((a-e)/e)
-                else:
-                    err.append(a-e)
-                #self.assertAlmostEqual(err, 0, 2)
-            print(f'\nfile: {f}')
-            print(f'min {min(err)}')
-            print(f'max {max(err)}')
-            print(f'mean {statistics.mean(err)}')
-            print(f'median {statistics.median(err)}')
-            print(f'stdev {statistics.stdev(err)}\n')
-            self.assertEqual(statistics.median(err), 0.0)
-            self.assertAlmostEqual(statistics.mean(err), 0, 6)
+                # Compare the test's output with the expected output
+                out_dir = os.path.join(os.path.dirname(work_dir), f'tests/{name}/output/')
+                exp_dir = os.path.join(out_dir, 'expected/')
+                act_dir = os.path.join(out_dir, 'actual/')
+                files = [f for f in os.listdir(exp_dir) if os.path.isfile(os.path.join(exp_dir, f))
+                        and f.endswith('.txt')]
+                for f in files:
+                    exp_fpath = os.path.join(exp_dir, f)
+                    act_fpath = os.path.join(act_dir, f)
+                    self.assertTrue(os.path.isfile(os.path.join(act_dir, f)))
+                    with open(exp_fpath) as f:
+                        exp_items = re.findall(r'\S+', f.read())
+                    with open(act_fpath) as f:
+                        act_items = re.findall(r'\S+', f.read())
+                    self.assertEqual(len(act_items), len(exp_items))
+                    err = []
+                    for (exp_item, act_item) in zip(exp_items, act_items):
+                        try:
+                            e = literal_eval(exp_item)
+                            a = literal_eval(act_item)
+                        except (ValueError, TypeError, SyntaxError, MemoryError):
+                            continue
+                        if e and a:
+                            err.append((a-e)/e)
+                        else:
+                            err.append(a-e)
+                        #self.assertAlmostEqual(err, 0, 2)
+                    print(f'\nfile: {f}')
+                    print(f'min {min(err)}')
+                    print(f'max {max(err)}')
+                    print(f'mean {statistics.mean(err)}')
+                    print(f'median {statistics.median(err)}')
+                    print(f'stdev {statistics.stdev(err)}\n')
+                    self.assertEqual(statistics.median(err), 0.0)
+                    self.assertAlmostEqual(statistics.mean(err), 0, 6)
 
 
 if __name__ == '__main__':
